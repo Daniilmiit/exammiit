2abdf3d1ccdf203b872e70c4053471b1a6a43f47
equillibrium <kotobars@gmail.com>
2023-12-15 18:22:33 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,17 @@
+﻿#!/bin/bash
+
+# Add Microsoft package repository
+wget https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+
+# Install .NET 6 SDK
+apt-get update
+apt-get install -y apt-transport-https
+apt-get update
+apt-get install -y dotnet-sdk-6.0
+
+# Clean up
+rm packages-microsoft-prod.deb
+
+# Verify installation
+dotnet --version
