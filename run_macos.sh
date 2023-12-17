048e8537143f2d7ff55b6838b4c7b6937b8564fc
Мыльцев Александр Михайлович <myltsev_a@tass.ru>
2023-12-17 18:46:39 +0300

    macos progress

---
+++ run_macos.sh
@@ -0,0 +1,17 @@
+﻿# задаём переменные
+service_name="miitexam.service"
+site_folder="/var/www/miitexam"
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
+source ~/.zshrc
+brew install dotnet@6
+brew install nginx
+
+export DOTNET_ROOT="/usr/local/opt/dotnet@6/libexec"
+echo 'export PATH="/usr/local/opt/dotnet@6/bin:$PATH"' >> ~/.zshrc
+source ~/.zshrc
+dotnet --version
+
+dotnet restore
+dotnet publish -c Release -o out app.csproj
