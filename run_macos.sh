b234f627b7a38015c3a7c09c2409d8a12806f172
Мыльцев Александр Михайлович <myltsev_a@tass.ru>
2023-12-17 19:12:18 +0300

    Update run_macos.sh

--- run_macos.sh
+++ run_macos.sh
@@ -1,7 +1,4 @@
 ﻿#!/bin/bash
-# задаём переменные
-service_name="miitexam.service"
-site_folder="/usr/www/miitexam"
 
 rm -rf out/
 
@@ -17,6 +14,9 @@ brew update
 source ~/.zshrc
 brew install dotnet@6
 brew install nginx
+
+cp nginx-macos.conf /usr/local/etc/nginx/nginx.conf
+brew services restart nginx
 
 export DOTNET_ROOT="/usr/local/opt/dotnet@6/libexec"
 echo 'export PATH="/usr/local/opt/dotnet@6/bin:$PATH"' >> ~/.zshrc
